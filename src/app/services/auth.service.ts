1c6c518248832de43366a5d3888072ef125e25b5
raffa96 <pocho962010@hotmail.it>
2019-11-28 16:08:16 +0100

    Set environment variables

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,18 @@
-import { IUser } from './../interfaces/user.interface';
-import { IJWT } from './../interfaces/jwt.interfaces';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { User } from './../classes/user.class';
-import { Injectable, Output, EventEmitter } from '@angular/core';
+import { IUser } from "./../interfaces/user.interface";
+import { IJWT } from "./../interfaces/jwt.interfaces";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders
+} from "@angular/common/http";
+import { User } from "./../classes/user.class";
+import { Injectable, Output, EventEmitter } from "@angular/core";
+import { environment } from "../../environments/environment";
 
 @Injectable()
 export class AuthService {
-  private apiAuthURL = 'http://localhost:8000/api/auth/';
-
   private authHeader = new HttpHeaders({
-    Authorization: 'Bearer ' + this.getToken()
+    Authorization: "Bearer " + this.getToken()
   });
 
   private isUserLogged = false;
@@ -20,10 +23,10 @@ export class AuthService {
 
   @Output() userLoggedOut = new EventEmitter<User>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUser(): IUser {
-    const data = JSON.parse(localStorage.getItem('user'));
+    const data = JSON.parse(localStorage.getItem("user"));
 
     const user = new User();
 
@@ -37,7 +40,7 @@ export class AuthService {
   }
 
   getToken() {
-    return localStorage.getItem('token');
+    return localStorage.getItem("token");
   }
 
   getAuthHeader(): HttpHeaders {
@@ -45,7 +48,7 @@ export class AuthService {
   }
 
   isUserLoggedIn() {
-    this.isUserLogged = !!localStorage.getItem('token');
+    this.isUserLogged = !!localStorage.getItem("token");
 
     return this.isUserLogged;
   }
@@ -57,45 +60,49 @@ export class AuthService {
 
     user.email = email;
 
-    this.http.post(this.apiAuthURL + 'signup', { name, email, password }).subscribe(
-      (response: IJWT) => {
-        localStorage.setItem('token', response.access_token);
+    this.http
+      .post(environment.APIAUTH + "signup", { name, email, password })
+      .subscribe(
+        (response: IJWT) => {
+          localStorage.setItem("token", response.access_token);
 
-        localStorage.setItem('user', JSON.stringify(response));
+          localStorage.setItem("user", JSON.stringify(response));
 
-        this.userSignedUp.emit(user);
-      },
-      (httpResponse: HttpErrorResponse) => {
-        alert(httpResponse.message);
-      }
-    );
+          this.userSignedUp.emit(user);
+        },
+        (httpResponse: HttpErrorResponse) => {
+          alert(httpResponse.message);
+        }
+      );
   }
 
   login(email: string, password: string) {
-    this.http.post(this.apiAuthURL + 'login', { email, password }).subscribe(
-      (response: IJWT) => {
-        localStorage.setItem('token', response.access_token);
+    this.http
+      .post(environment.APIAUTH + "login", { email, password })
+      .subscribe(
+        (response: IJWT) => {
+          localStorage.setItem("token", response.access_token);
 
-        localStorage.setItem('user', JSON.stringify(response));
+          localStorage.setItem("user", JSON.stringify(response));
 
-        const user = new User();
+          const user = new User();
 
-        user.name = response.name;
+          user.name = response.name;
 
-        user.email = response.email;
+          user.email = response.email;
 
-        this.userLoggedIn.emit(user);
-      },
-      (httpResponse: HttpErrorResponse) => {
-        alert(httpResponse.message);
-      }
-    );
+          this.userLoggedIn.emit(user);
+        },
+        (httpResponse: HttpErrorResponse) => {
+          alert(httpResponse.message);
+        }
+      );
   }
 
   logout() {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
 
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
 
     this.userLoggedOut.emit();
 
